cf6c628e37c53f73bf56ff2e0e9cf25cf16fa2a9
gezish <enggezahegn.w@gmail.com>
2022-05-10 14:45:57 +0300

    Histogram module  module

--- scripts/data_visualizer.py
+++ scripts/data_visualizer.py
@@ -16,3 +16,12 @@ def plotly_plot_pie(df, column, limit=None):
     fig = px.pie(a, values='count', names=column, title=f'Distribution of {column}s', width=800, height=500)
     fig.show()
 
+def plotly_plot_hist(df, column, color=['cornflowerblue']):
+    fig = px.histogram(
+            df,
+            x=column,
+            marginal='box',
+            color_discrete_sequence=color,
+            title=f'Distribution of {column}')
+    fig.update_layout(bargap=0.01)
+    fig.show()
