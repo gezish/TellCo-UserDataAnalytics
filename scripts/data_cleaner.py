c1e2bbb139bfe7ef8898e84c9ae972bcb3529f60
gezish <enggezahegn.w@gmail.com>
2022-05-10 13:56:05 +0300

    missing value by columen added

--- scripts/data_cleaner.py
+++ scripts/data_cleaner.py
@@ -47,3 +47,36 @@ def percent_missing_rows(df):
     # Calculate the percentage of missing rows
     print(round(((missing_rows/total_rows) * 100), 2), "%",
     "of the rows in the dataset contain atleast one missing value.")
+    
+def missing_values_table(df):
+    # Total missing values
+    mis_val = df.isnull().sum()
+
+    # Percentage of missing values
+    mis_val_percent = 100 * mis_val / len(df)
+
+    # dtype of missing values
+    mis_val_dtype = df.dtypes
+
+    # Make a table with the results
+    mis_val_table = pd.concat([mis_val, mis_val_percent, mis_val_dtype], axis=1)
+
+    # Rename the columns
+    mis_val_table_ren_columns = mis_val_table.rename(
+    columns = {0 : 'Missing Values', 1 : '% of Total Values', 2: 'Dtype'})
+
+    # Sort the table by percentage of missing descending and remove columns with no missing values
+    mis_val_table_ren_columns = mis_val_table_ren_columns[
+        mis_val_table_ren_columns.iloc[:,0] != 0].sort_values(
+    '% of Total Values', ascending=False).round(2)
+
+    # Print some summary information
+    print ("Your selected dataframe has " + str(df.shape[1]) + " columns.\n"
+        "There are " + str(mis_val_table_ren_columns.shape[0]) +
+          " columns that have missing values.")
+
+    if mis_val_table_ren_columns.shape[0] == 0:
+        return
+
+    # Return the dataframe with missing information
+    return mis_val_table_ren_columns
