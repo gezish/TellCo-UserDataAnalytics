fc2df7c7b1734e62b874236cb855033d33dbce0d
gezish <enggezahegn.w@gmail.com>
2022-05-10 12:45:22 +0300

    initial scripts

---
+++ scripts/data_cleaner.py
@@ -0,0 +1,23 @@
+import numpy as np
+import pandas as pd
+
+def convert_to_string(df, columns):
+    for col in columns:
+        df[col] = df[col].astype("string")
+
+def convert_to_numbers(df, columns):
+    for col in columns:
+        df[col] = pd.to_numeric(df[col])
+
+def convert_to_int(df, columns):
+    for col in columns:
+        df[col] = df[col].astype("int64")
+
+def convert_to_datetime(df, columns):
+    for col in columns:
+        df[col] = pd.to_datetime(df[col])
+
+def multiply_by_factor(df, columns, factor):
+    for col in columns:
+        df[col] = df[col] * factor
+
