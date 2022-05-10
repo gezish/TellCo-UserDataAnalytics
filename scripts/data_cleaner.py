8c6a51f942d490bbdf4cb066b02133f6a15f3f01
gezish <enggezahegn.w@gmail.com>
2022-05-10 12:51:56 +0300

    missing value counter added

--- scripts/data_cleaner.py
+++ scripts/data_cleaner.py
@@ -21,3 +21,16 @@ def multiply_by_factor(df, columns, factor):
     for col in columns:
         df[col] = df[col] * factor
 
+def percent_missing_values(df):
+
+    # Calculate total number of cells in dataframe
+    totalCells = np.product(df.shape)
+
+    # Count number of missing values per column
+    missingCount = df.isnull().sum()
+
+    # Calculate total number of missing values
+    totalMissing = missingCount.sum()
+
+    # Calculate percentage of missing values
+    print("The dataset contains", round(((totalMissing/totalCells) * 100), 2), "%", "missing values.")
