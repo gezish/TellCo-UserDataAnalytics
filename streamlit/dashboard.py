273e17f0273ca0432cb3b12bedb8e6130f14836c
gezish <enggezahegn.w@gmail.com>
2022-05-13 22:24:01 +0300

    dashboard test

---
+++ streamlit/dashboard.py
@@ -0,0 +1,17 @@
+import overview
+import user_engagement
+import user_experience
+import user_satisfaction
+import streamlit as st
+# from flask import Flask
+
+PAGES = {
+    "Data Overview": overview,
+    "User Engagement Analysis":  user_engagement,
+    "User Experience Analytics": user_experience,
+    "User Satisfaction Analysis": user_satisfaction,
+}
+
+selection = st.sidebar.radio("Go to page", list(PAGES.keys()))
+page = PAGES[selection]
+page.app()
