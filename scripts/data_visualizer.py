088535918bdc010c020cd22a190e417a41e595bd
gezish <enggezahegn.w@gmail.com>
2022-05-14 16:14:27 +0300

    chnage traks

--- scripts/data_visualizer.py
+++ scripts/data_visualizer.py
@@ -92,3 +92,15 @@ def hist(sr):
     x = ["Id: " + str(i) for i in sr.index]
     fig = px.histogram(x=x, y=sr.values)
     fig.show()
+    
+def mult_hist(sr, rows, cols, title_text, subplot_titles, interactive=False):
+    fig = make_subplots(rows=rows, cols=cols, subplot_titles=subplot_titles)
+    for i in range(rows):
+        for j in range(cols):
+            x = ["-> " + str(i) for i in sr[i+j].index]
+            fig.add_trace(go.Bar(x=x, y=sr[i+j].values), row=i+1, col=j+1)
+    fig.update_layout(showlegend=False, title_text=title_text)
+    if(interactive):
+        fig.show()
+    else:
+        return Image(pio.to_image(fig, format='png', width=1200))
